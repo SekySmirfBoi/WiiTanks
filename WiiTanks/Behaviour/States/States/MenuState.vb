96a90addc8fba50e499fb91355b95fda5e9c5f8a
SekySmirfBoi <kickbottomarlito2@gmail.com>
2024-11-21 14:59:44 +0000

    mdhrjgaeg

--- WiiTanks/Behaviour/States/States/MenuState.vb
+++ WiiTanks/Behaviour/States/States/MenuState.vb
@@ -10,8 +10,8 @@
 
         _uiManager = New UIManager()
 
-        _uiManager.AddComponent(New TextBlock(New Point(100, 100), "dick", SharedResources.BlankImage))
-        _uiManager.AddComponent(New Button(New Point(SharedResources.CentreWindowCoord.X - 40, 100), "ilugylku", New Size(80, 40),
+        _uiManager.AddComponent(New TextBlock(New Point(SharedResources.CentreWindowCoord.X - 30, 100), "dick", SharedResources.BlankImage))
+        _uiManager.AddComponent(New Button(New Point(SharedResources.CentreWindowCoord.X - 40, 150), "ilugylku", New Size(80, 40),
                                            Function() As Boolean
                                                MsgBox("fuck you")
                                            End Function))
--- WiiTanks/Behaviour/UI/Components/TextBlock.vb
+++ WiiTanks/Behaviour/UI/Components/TextBlock.vb
@@ -3,16 +3,18 @@
 
     Private _backImage As Image
     Private _text As String
+    Private _font As Font
 
-    Sub New(position As Point, text As String, backgournd As Image)
+    Sub New(position As Point, text As String, backgournd As Image, fon As Font)
         MyBase.New(position)
         _text = text
         _backImage = backgournd
+        _font = fon
     End Sub
 
     Public Overrides Sub Render(graohics As Graphics)
         graohics.DrawImage(_backImage, p_loc)
-        graohics.DrawString(_text, New Font("Ariel", 18), New Pen(Color.Gray).Brush, p_loc)
+        graohics.DrawString(_text, _font, New Pen(Color.Gray).Brush, p_loc)
     End Sub
 
     Public Overrides Sub Click(MouseCords As Point)
